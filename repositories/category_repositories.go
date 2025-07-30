cb70ea433b5b609449d973daead0f1efab5b52e0
frank.shen <frank.shwn@newpos.tw>
2025-07-30 20:40:15 +0800

    [Feature] 完善商品與分類的 CRUD API，新增查詢、更新及刪除功能，並在路由中加入相應的路由設定

--- repositories/category_repositories.go
+++ repositories/category_repositories.go
@@ -8,3 +8,19 @@ import (
 func CreateCategory(category *models.Category) error {
 	return database.DB.Create(category).Error
 }
+
+func GetCategories(categories *[]models.Category) error {
+	return database.DB.Preload("Children").Find(categories).Error
+}
+
+func GetCategoryByID(id string, category *models.Category) error {
+	return database.DB.Preload("Children").First(category, id).Error
+}
+
+func UpdateCategory(category *models.Category) error {
+	return database.DB.Save(category).Error
+}
+
+func DeleteCategory(id string) error {
+	return database.DB.Delete(&models.Category{}, id).Error
+}
